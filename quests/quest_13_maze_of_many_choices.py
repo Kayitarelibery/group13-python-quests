0e4ca621f033a76d92223e8a0d2c62c6be64eb68
sanol-1 <sanol-1>
2026-06-29 10:23:31 -0700

    Quests 11-15

--- quests/quest_13_maze_of_many_choices.py
+++ quests/quest_13_maze_of_many_choices.py
@@ -2,11 +2,11 @@
 
 score = int(input("Enter your score (0-100): "))
 
-if score >= 90:
+if score >= 90 and score <=100:
     print("A")
-elif score >= 80:
+elif score >= 80 and score <=89:
     print("B")
-elif score >= 70:
+elif score >= 70 and score <= 79:
     print("C")
 else:
     print("Needs Improvement")
