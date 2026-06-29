945dadafb0e74d11b4596d87c2169d3225215c77
crugwiro-sudo <c.rugwiro@alustudent.com>
2026-06-29 21:40:55 +0200

    Quest 15: The Nested Riddle - nested if statements

--- quests/quest_15_nested_riddle.py
+++ quests/quest_15_nested_riddle.py
@@ -1,12 +1,13 @@
-#!usr/bin/env python3
+# Quest 15: The Nested Riddle
+# Goal: use an if statement inside another if statement
 
 direction = input("Do you go left or right? ")
 
 if direction == "left":
     action = input("Do you swim or wait? ")
     if action == "swim":
-        print("You found a treasure!")
+        print("You found a treasure chest!")
     else:
-        print("You found no treasure!")
+        print("You wait and nothing happens.")
 else:
-    print("You wander off and never find anything interesting.")
+    print("You wander off and get lost.")
