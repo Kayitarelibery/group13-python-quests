6186455a4ea6f95e07fbff027029adfd0a7f9374
acyubahiroo <a.cyubahiro@alustudent.com>
2026-06-29 11:44:01 -0700

    Member 2, Quests 6-10

---
+++ quests/quest_08_the_potion_brewer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+scales = 3 * 10
+tears = 5 * 3
+total = scales + tears
+print(f"Total cost: ", total)
---
+++ quests/quest_09_the_greedy_goblin.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+gold = 27
+friends = 4
+each_gets = gold // friends
+goblin_keeps = gold % friends
+print(f"Each friend gets:", each_gets)
+print(f"The goblin keeps:", goblin_keeps)
