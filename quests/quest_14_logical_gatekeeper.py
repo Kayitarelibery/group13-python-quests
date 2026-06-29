df27f04613d531b8b9d76e00e772a834e4a5ed20
sanol-1 <sanol-1>
2026-06-29 13:59:04 -0700

    Quest 11 to 15

--- quests/quest_14_logical_gatekeeper.py
+++ quests/quest_14_logical_gatekeeper.py
@@ -6,6 +6,6 @@ gold = int(input("Enter your gold coins: "))
 if age >= 18 and gold >= 20:
     print("You may enter the club.")
 elif age < 18 and gold < 20:
-    print("You cannot enter the club.")
+    print("Not allowed in the club.")
 else:
     print("Invalid input")
